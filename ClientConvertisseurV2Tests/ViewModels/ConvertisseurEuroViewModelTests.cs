78b31640e85c0220e9352d89c2c21a8f0cdabe95
gallottm <gallottm@D352-S16>
2024-01-30 09:44:03 +0100

    test

--- ClientConvertisseurV2Tests/ViewModels/ConvertisseurEuroViewModelTests.cs
+++ ClientConvertisseurV2Tests/ViewModels/ConvertisseurEuroViewModelTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClientConvertisseurV2.Models;
 
 namespace ClientConvertisseurV2.ViewModels.Tests
 {
@@ -44,11 +45,47 @@ namespace ClientConvertisseurV2.ViewModels.Tests
         {
             //Arrange
             ConvertisseurEuroViewModel convertisseurEuro = new ConvertisseurEuroViewModel();
+            List<Devise> liste = new List<Devise>
+            {
+                 new Devise { Id = 1, NomDevise = "Dollar", Taux =  1.08 },
+                new Devise { Id = 2, NomDevise = "Franc Suisse", Taux = 1.07 },
+                new Devise { Id = 3, NomDevise = "Yen", Taux = 120 },
+            };
             //Act
             convertisseurEuro.GetDataOnLoadAsync();
             Thread.Sleep(1000);
             //Assert
-            Assert.AreEqual(3, convertisseurEuro.Devises.Count);
+            CollectionAssert.AreEqual(liste, convertisseurEuro.Devises);
         }
+
+        /// <summary>
+        /// Test conversion OK
+        /// </summary>
+        [TestMethod()]
+        public void ActionSetConversionTest()
+        {
+            //Arrange
+            ConvertisseurEuroViewModel convertisseurEuro = new ConvertisseurEuroViewModel();
+            convertisseurEuro.MontantEuro = 100;
+            Devise d = new Devise(4, "Temporaire", 1.07);
+            convertisseurEuro.SelectedDevise = d;
+            //Act
+            convertisseurEuro.ActionSetConversion();
+            //Assert
+            Assert.AreEqual(107, convertisseurEuro.MontantDevise);
+        }
+
+        [TestMethod()]
+        public void GetDataOnLoadAsyncTest_WSServiceNonDemarre()
+        {
+            //Arrange
+            ConvertisseurEuroViewModel convertisseurEuro = new ConvertisseurEuroViewModel();
+            //Act
+            convertisseurEuro.GetDataOnLoadAsync();
+            Thread.Sleep(1000);
+            //Assert
+            Assert.IsNull(convertisseurEuro.Devises, "L'api est démarrée.");
+        }
+
     }
 }
